055c324464a75034c213647a4fb737e0355a6d71
Tuba Eraslan <tubaeraslan193@gmail.com>
2024-06-29 18:09:03 +0300

    .

--- lesson1/src/App.jsx
+++ lesson1/src/App.jsx
@@ -6,42 +6,41 @@ import './App.css'
 function App() {
   //jsx{}
   //js kodları
+
   let a = 15;
-  const firstName = "Tuba";
+  const firstName = "tuba";
 
   let vize1 = 60;
   let vize2 = 20;
+  let ort = (vize1 + vize2) / 2;
 
-  let sonuc = false;
+  let isimler = ["tuba", "ali", "ahmet", "ayşe"];
 
-  let names = ["Tuba","Enes","Ayşe","Kübra","Adem"];
   return (
     //html kodları
-    //kkk
     <div>
-      {/* <p>a değişkeninin değeri : {a}</p>
-      <p>ad : {firstName}</p> */}
+      {/* <p>a değişkeni: {a}</p>
+      <p>müşterinin adı: {firstName}</p> */}
 
+      {/* <p>ortalama: {(vize1+vize2)/2}</p> */}
 
-      {/* <p>Ortalama: {(vize1+vize2)/2}</p> */}
-      {/* {sonuc ? <p>Ehliyet alabilirsin</p> : <p>Ehliyet alamazsın</p>} */}
+      {/* {ort>=50 ? <p>gectin</p> : <p>kaldın</p>} */}
 
-      {/* {
-        (vize1+vize2)/2 >= 50 ? <p>Dersten geçtin</p> : <p>Dersten kaldın</p>
-      } */}
+      {
+        // (vize1+vize2)/2 >=50 ? <p>gectin</p> : <p>kaldın</p>
+      }
 
-      {/* {
-        names.map((name,index)=>(
-           <div key={index} style={{
-            backgroundColor:"orange",
-            border:"1px solid black"
-           }}>
-            {name}
-           </div>
+      {
+        isimler.map((isim, index) => (
+          <div key={index} style={{
+            backgroundColor: 'orange',
+            color: 'red',
+            border: '1px solid black'
+          }}>
+            {isim}
+          </div>
         ))
-      } */}
-
-      
+      }
     </div>
   )
 }
